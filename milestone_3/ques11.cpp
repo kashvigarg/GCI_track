2260624b539dc46dbcdf12a3f5c0288a40e7d76e
kashvigarg <kashvigarg02@gmail.com>
2022-03-26 15:52:19 +0530

    random related ans formats

--- milestone_3/ques11.cpp
+++ milestone_3/ques11.cpp
@@ -1,2 +1,28 @@
 // Write a C program to find first and last digit of a number.
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, num1, i, first, last;
+    cout << "Enter any number:" << endl;
+    cin >> num;
+    if (num > 9)
+    {
+        last = (num % 10);
+        while (1)
+        {
+            num1 = num;
+            num = num / 10;
+            if (num == 0)
+            {
+                cout << "The first digit is " << num1 << endl;
+                break;
+            }
+        }
+        cout << "The last digit is " << last << endl;
+    }
+    else
+        cout << "You have entered a single digit number" << endl;
+}
