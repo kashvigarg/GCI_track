da97b022de5391982dfa781e53c742599aff0aa2
kashvigarg <kashvigarg02@gmail.com>
2022-05-18 01:00:27 +0530

    data arrangement

--- milestone_6/functions/codeforwin/ques5.cpp
+++ milestone_6/functions/codeforwin/ques5.cpp
@@ -1,2 +1,13 @@
 /*Write a C program to check whether a number is prime, Armstrong or perfect number using functions.
 */
+
+#include <iostream>
+using namespace std;
+#include <string>
+#include <cmath>
+   
+int main()
+{
+    
+return 0;
+}
